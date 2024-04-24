83f5b8249dc3ed3a71f6722c42e2337987aad63b
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-24 23:43:47 +0300

    initial commit

---
+++ src/main/java/com/agan/fundamentalsddl_dml/FundamentalsDdlDmlHwUsersTableApplication.java
@@ -0,0 +1,13 @@
+package com.agan.fundamentalsddl_dml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsDdlDmlHwUsersTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsDdlDmlHwUsersTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agan/fundamentalsddl_dml/FundamentalsDdlDmlHwUsersTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agan.fundamentalsddl_dml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsDdlDmlHwUsersTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
