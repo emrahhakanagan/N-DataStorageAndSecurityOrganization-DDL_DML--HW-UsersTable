f378bb5ca8504964dbd269df481bff28e47df3a9
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-24 23:46:34 +0300

    sql script files added

---
+++ sql-files/persons-schema-searching-by-age.sql
@@ -0,0 +1,3 @@
+SELECT * FROM persons WHERE age > 27 ORDER BY age DESC;
+
+SELECT name, surname FROM persons WHERE country_of_living = 'Russia';
---
+++ sql-files/persons-schema-searching-by-country.sql
@@ -0,0 +1,1 @@
+SELECT name, surname FROM persons WHERE country_of_living = 'Russia';
